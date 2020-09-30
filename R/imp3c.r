fadc9fa49c5878509df2bb0986812309d26895e9
Lucille Delisle <lucille.delisle@epfl.ch>
2020-09-30 12:05:15 +0200

    implement gtrack.create_redb_tracks

--- R/imp3c.r
+++ R/imp3c.r
@@ -12,7 +12,7 @@
 #' 
 #' @export 
 p4cBuildRequirements <- function(output_dir, genome="hg19", reseq="GATC", 
-        dataset_keys="http://www.wisdom.weizmann.ac.il/~omersch/dataset_keys.txt")
+                                 dataset_keys="http://www.wisdom.weizmann.ac.il/~omersch/dataset_keys.txt")
 {
     datasets <- read.table(url(dataset_keys), header=TRUE, stringsAsFactors=FALSE)
     tf <- tempfile()
@@ -70,9 +70,9 @@ p4cDumpConfFiles <- function(conf_dir)
 #' 
 #' @export 
 p4cLoadConfFiles <- function(conf_dir, 
-    paths_conf=file.path(conf_dir, "paths.conf"),
-    pipeline_conf=file.path(conf_dir, "pipeline.conf"),
-    p4c_conf=file.path(conf_dir, "p4c.conf"))
+                             paths_conf=file.path(conf_dir, "paths.conf"),
+                             pipeline_conf=file.path(conf_dir, "pipeline.conf"),
+                             p4c_conf=file.path(conf_dir, "p4c.conf"))
 {
     op <- options()
     all_paths <- list(
@@ -90,7 +90,7 @@ p4cLoadConfFiles <- function(conf_dir,
     {
         
         message(sprintf("%i parameters were already defined in this sessions and they will overwritten!", 
-                sum(setted)))
+                        sum(setted)))
     }
     
     options(op.p4c)
@@ -98,7 +98,7 @@ p4cLoadConfFiles <- function(conf_dir,
     .checkConf()
     gset_input_mode(autocompletion = FALSE, interactive = FALSE)
     gsetroot(getOption("TG3C.trackdb"), rescan = TRUE)
-
+    
     invisible()
 }
 
@@ -111,7 +111,7 @@ p4cLoadConfFiles <- function(conf_dir,
     {
         params <- c(params, additional_params)
     }
-
+    
     if (!all( params %in% names(options()) )) 
     {
         stop("Missing conf parameters! Have you loaded the files with 4cLoadConfFiles?")
@@ -139,7 +139,7 @@ p4cLoadConfFiles <- function(conf_dir,
 #' 
 #' @export 
 p4cCreate4CseqTrack = function(sample_ids = NULL, track_desc = "4C track",  verbose = TRUE,  
-    overwrite.if.exist = FALSE, groot = NULL)
+                               overwrite.if.exist = FALSE, groot = NULL)
 {
     .checkConf()
     options(gparam.type = "string")
@@ -148,7 +148,7 @@ p4cCreate4CseqTrack = function(sample_ids = NULL, track_desc = "4C track",  verb
     paths_conf_path    <- getOption("TG3C.paths_conf_path")
     perl_scripts_path <- system.file("perl", package="umi4cPackage")
     
-     
+    
     # read table data from tables
     baits_tab_nm <- getOption("TG3C.baits_tab")
     samples_tab_nm <- getOption("TG3C.samples_tab")
@@ -196,7 +196,7 @@ p4cCreate4CseqTrack = function(sample_ids = NULL, track_desc = "4C track",  verb
     for (foc_ndx in sample_ids)
     {
         row_idx <- which(samples_tab$Sample_ID == foc_ndx)
-
+        
         # Test existence
         if (length(row_idx) == 0) 
         {
@@ -211,7 +211,7 @@ p4cCreate4CseqTrack = function(sample_ids = NULL, track_desc = "4C track",  verb
         exp_nm <- samples_tab$Experiment_name[row_idx]
         sample_nm <- samples_tab$Sample_name[row_idx]
         baits_idx <- as.numeric(strsplit(as.character(samples_tab$Bait_IDs[row_idx]), 
-            split = ",")[[1]])
+                                         split = ",")[[1]])
         
         foc_ndx_track_name = sprintf("%s_%s_%s", base_track_nm, exp_nm, sample_nm)
         
@@ -241,7 +241,7 @@ p4cCreate4CseqTrack = function(sample_ids = NULL, track_desc = "4C track",  verb
                 message("Skipping processing of track ", foc_ndx_track_name_bait, ": already exists.\n")
                 existing_tracks <- existing_tracks + 1
                 next
-             } 
+            } 
         }
         if (existing_tracks == length(baits_idx)) 
         {
@@ -250,12 +250,12 @@ p4cCreate4CseqTrack = function(sample_ids = NULL, track_desc = "4C track",  verb
         }
         
         cmd = sprintf("perl %s/%s @%s @%s -TG3C.sample_id %s -TG3C.perl_scripts_path %s\n", 
-            perl_scripts_path, 
-            imp_3C_pipe_pl, 
-            pipeline_conf_path, 
-            paths_conf_path, 
-            foc_ndx, 
-            perl_scripts_path)
+                      perl_scripts_path, 
+                      imp_3C_pipe_pl, 
+                      pipeline_conf_path, 
+                      paths_conf_path, 
+                      foc_ndx, 
+                      perl_scripts_path)
         if (verbose)
         {
             message(cmd, "\n")
@@ -264,7 +264,7 @@ p4cCreate4CseqTrack = function(sample_ids = NULL, track_desc = "4C track",  verb
         if (ret)
         {
             stop("An error occured while running the pipeline on index ", foc_ndx, 
-                " ")
+                 " ")
         }
         
         status_fn = sprintf("%s/%s.%s/done_ok", work_dir, exp_nm, sample_nm)
@@ -275,19 +275,19 @@ p4cCreate4CseqTrack = function(sample_ids = NULL, track_desc = "4C track",  verb
             return(NA)
         }
         
-       
-    # Genereate a track for each bait
+        
+        # Genereate a track for each bait
         for (bait_id in baits_idx)
         {
             bait_row_idx <- which(baits_tab$Bait_ID == bait_id)
             bait_name <- baits_tab$Bait_name[bait_row_idx]
             contacts_fn <- sprintf("%s/%s.%s/adj.%s", work_dir, exp_nm, sample_nm, 
-              bait_name)
+                                   bait_name)
             foc_ndx_track_name_bait <- paste0(foc_ndx_track_name, "_", bait_name)
             message("Will import ", foc_ndx_track_name_bait, " from ", contacts_fn, 
-              " fends at ", fends_fn)
+                    " fends at ", fends_fn)
             gtrack.2d.import_contacts(foc_ndx_track_name_bait, description = track_desc, 
-              contacts = contacts_fn, fends = fends_fn)
+                                      contacts = contacts_fn, fends = fends_fn)
             bait_chr <- baits_tab$Bait_chr[bait_row_idx]
             bait_coord <- baits_tab$Bait_coord[bait_row_idx]
             gtrack.attr.set(foc_ndx_track_name_bait, attr = "Bait_name", value = bait_name)
@@ -298,39 +298,55 @@ p4cCreate4CseqTrack = function(sample_ids = NULL, track_desc = "4C track",  verb
     }
 }
 
-################################
-# Function used to create redb tracks for other reseqs
-# Not ready yet!
-################################
-gtrack.create_redb_tracks = function(re_seq, redb_params_fn, verbose = FALSE)
+
+gtrack.create_redb_tracks = function(re_seq, redb_params_fn, verbose = TRUE)
 {
     options(gparam.type = "string")
     Sys.setenv(PERL_BADLANG = 0)
+    # Set the parameters in the file:
     op.new = init_params(redb_params_fn)
     options(op.new)
     
-    gen_re_frags_pl = getOption("TG3C.gen_re_frags_pl", NA)
-    re_frags_to_fends_pl = getOption("TG3C.re_frags_to_fends_pl", NA)
+    # Get the path of the perl scripts
+    gen_re_frags_pl <- grep("build_re_db.pl", list.files(.libPaths(),
+                                                         recursive = T,
+                                                         full.names = T),
+                            value = T)
+    re_frags_to_fends_pl <- grep("re_frags_to_fends_tab.pl",
+                                 list.files(.libPaths(),
+                                            recursive = T,
+                                            full.names = T),
+                                 value = T)
+    
     mapab_track = getOption("TG3C.mapab_track", NA)
     re_workdir = getOption("TG3C.re_workdir", NA)
     chrom_key = getOption("TG3C.chrom_seq_key", NA)
     
     # Check existance
-    all_set <- all(is.na(c(gen_re_frags_pl, re_frags_to_fends_pl, mapab_track, re_workdir, chrom_key)))
+    all_set <- all(!is.na(c(mapab_track, 
+                            re_workdir, chrom_key))) &
+        length(gen_re_frags_pl) == 1 &
+        length(re_frags_to_fends_pl) == 1
     
     if (!all_set)
     {
-        message("ERROR: missing configuration options\n")
-        return(NA)
+        stop("ERROR: missing configuration options\n")
     }
-    
     # create the frags tables
-    message("constructing table from genomic sequence\n")
+    if (verbose){
+        message("constructing table from genomic sequence\n")
+    }
+    # Create the directory if it does not exists
+    dir.create(re_workdir, recursive = T, showWarnings = F)
     cmd = sprintf("perl %s %s %s %s", gen_re_frags_pl, re_seq, chrom_key, re_workdir)
     system(cmd)
     
+    
     # import the frags as sparse tracks
-    message("importing fragments as sparse tracks")
+    if (verbose){
+        message("importing fragments as sparse tracks")
+    }
+    
     # create redb dir
     gdir.create("redb", showWarnings = FALSE)
     
@@ -340,20 +356,23 @@ gtrack.create_redb_tracks = function(re_seq, redb_params_fn, verbose = FALSE)
     flen_file = sprintf("%s/%s_flen", re_workdir, re_seq)
     gc_file = sprintf("%s/%s_gc", re_workdir, re_seq)
     fragmap_file = sprintf("%s/%s_map", re_workdir, re_seq)
-    message("Importning tracks flen_track ", flen_track, " gc_track ", gc_track)
+    if (verbose){
+        message("Importing tracks flen_track ", flen_track, " gc_track ", gc_track)
+    }
     gtrack.import(flen_track, paste(re_seq, "fragment length"), flen_file, 0)
     gtrack.import(gc_track, paste(re_seq, "gc content"), gc_file, 0)
-    
     # annotate mapability
-    message("generating mapability data for fraqments, using ", mapab_track)
-    gtrack.create(fragmap_track, paste(re_seq, "fragment mapability"), as.character(mapab_track), 
-        iterator = flen_track)
-    
+    if (verbose){
+        message("generating mapability data for fraqments, using ", mapab_track)
+    }
+    gtrack.create(fragmap_track, paste(re_seq, "fragment mapability"),
+                  as.character(mapab_track), iterator = flen_track)
     # dump mapability frags to text
-    message("writing mapability text file to re workdir")
+    if (verbose){
+        message("writing mapability text file to re workdir")
+    }
     gextract(fragmap_track, ALLGENOME, iterator = fragmap_track, file = fragmap_file)
-    
-    # sperate frags to fends table
+    # separate frags to fends table
     message("writing fend tables from frag tables")
     cmd = sprintf("perl %s %s %s", re_frags_to_fends_pl, re_workdir, re_seq)
     system(cmd)
@@ -365,12 +384,38 @@ gtrack.create_redb_tracks = function(re_seq, redb_params_fn, verbose = FALSE)
     fe_flen_file = sprintf("%s/fe%s_flen", re_workdir, re_seq)
     fe_gc_file = sprintf("%s/fe%s_gc", re_workdir, re_seq)
     fe_fragmap_file = sprintf("%s/fe%s_map", re_workdir, re_seq)
-    message(sprintf("Importng fend tracks: %s, %s, %s", fe_flen_track, fe_gc_track, 
-        fe_fragmap_track))
+    # I noticed that when I am using contigs 
+    # they may have the re_seq at the beginning: for example in galGal5:
+    # >chr17_NT_462588v1_random
+    # GGATCCAACAGccaaacagagcaaaaccaaaggaaagtgaggaaaaataa
+    # Then 2-3 is both the start and the end
+    # of the first fragment. This induce duplicated lines in feFILES.
+    nb.duplicates <- system(paste("cat", fe_flen_file,
+                                  "| uniq -d | wc -l > nb.duplicates"),
+                            intern = TRUE)
+    if (nb.duplicates > 0){
+        # If there are duplicates new files will be created ended by
+        # wd (without duplicates)
+        temp.fe_flen_file <- sprintf("%s/fe%s_flen.wd", re_workdir, re_seq)
+        system(paste("cat", fe_flen_file, "| uniq >", temp.fe_flen_file))
+        fe_flen_file <- temp.fe_flen_file
+        
+        temp.fe_gc_file <- sprintf("%s/fe%s_gc.wd", re_workdir, re_seq)
+        system(paste("cat",fe_gc_file, "| uniq >", temp.fe_gc_file))
+        fe_gc_file <- temp.fe_gc_file
+        
+        temp.fe_fragmap_file<-sprintf("%s/fe%s_map.wd", re_workdir, re_seq)
+        system(paste("cat", fe_fragmap_file, "| uniq >", temp.fe_fragmap_file))
+        fe_fragmap_file <- temp.fe_fragmap_file
+    }
+    if (verbose){
+        message(sprintf("Importng fend tracks: %s, %s, %s", fe_flen_track, fe_gc_track, 
+                        fe_fragmap_track))
+    }
     gtrack.import(fe_flen_track, paste(re_seq, "fragment end length"), fe_flen_file, 
-        0)
+                  0)
     gtrack.import(fe_gc_track, paste(re_seq, "fragment end gc"), fe_gc_file, 0)
     gtrack.import(fe_fragmap_track, paste(re_seq, "fragment end mapability"), fe_fragmap_file, 
-        0)
+                  0)
 }
 
